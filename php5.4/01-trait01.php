188b4125b5d257463b8aa775f1ca4932ed9aa28d
artyamamoto <yamamoto.k@art-bank.co.jp>
2013-10-29 13:02:08 +0900

    traitのサンプル修正 番号を変更（traitのファイルが増えるため）

--- php5.4/01-trait01.php
+++ php5.4/01-trait01.php
@@ -1,5 +1,20 @@
 <?php
 //=== trait ===========
+/**
+ * Class Singleton
+ * クラスをsingletonにする traitです。
+ *
+ * trait は機能はクラスとほぼ同じですが、以下のような機能があります。
+ *   - サブクラスで利用する場合、use文を使う
+ *   - 多重継承が可能
+ *
+ *   - 関数が重複する場合、どちらが優先されるかを選択しなければならない
+ *   - 関数のエイリアスが定義できる。
+ *   - trait で定義された関数はclassに優先する
+ *
+ *   - その他、サブクラスで関数の可視性（public/privateなど）を変更できるなど、
+ *     通常のクラスよりも柔軟な設定が可能。
+ */
 trait Singleton {
 	protected function __construct() {}
 	public static function getInstance() {
@@ -13,28 +28,16 @@ trait Singleton {
 		throw new RuntimeException("this instance can't be replicated.");
 	}
 }
-//=== Deamon ================
-class Deamon {
-	public function curse() {
-		echo __METHOD__, "\n";
-	}
-}
-//=== Kogure =================
-class Kogure extends Deamon  {
+//=== Sample =================
+class Sample {
 	use Singleton;
-	
-	public function sing() {
-		echo __METHOD__,"\n";
-	}
 }
-
-echo "<pre>";
-
-$Kogure = Kogure::getInstance();
-$Kogure->curse();
-$Kogure->sing();
-
-
-echo "</pre>";
+//=== main ====================
+/**
+ * Sampleクラスでは Singleton trait を use しているので
+ * getInstance()メソッドが使えます。
+ */
+$i = Sample::getInstance();
+var_dump($i);
 
 ?>
